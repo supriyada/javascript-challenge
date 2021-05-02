ac0f31df7fe46d3784806a345b3115ce9b7ce92b
supriyada <supriyada@gmail.com>
2021-05-01 22:40:16 -0400

    Added all data into table

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,37 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+//console.log(tableData);
+
+var ufoTableBody = d3.select("tbody");
+
+//appends a table to the web page and then adds new rows of data for each UFO sighting
+tableData.forEach( (ufoReport) =>{
+    var ufoRow = ufoTableBody.append("tr");
+
+    Object.entries(ufoReport).forEach(([key,value]) => {
+        //console.log(key,value);
+        var ufoCell = ufoRow.append("td");
+        ufoCell.text(value);
+    });
+});
+
+//search through the `date/time` column to find rows that match user input
+var ufoButton = d3.select("#filter-btn");
+var ufoForm = d3.select("#form");
+
+ufoButton.on("click", runEnter);
+ufoForm.on("submit", runEnter);
+
+function runEnter(){
+    d3.event.preventDefault();
+
+    var inputElement = d3.select("#datetime");
+
+    var filterDate = inputElement.property("value");
+
+    //console.log(inputValue);
+    
+    var filteredTable = tableData.filter(ufoSightings => ufoSightings.datetime === filterDate);
+    console.log(filteredTable);
+}
