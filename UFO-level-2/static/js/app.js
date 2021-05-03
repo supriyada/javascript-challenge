09271c42814c6a3950a3656dcf65264ca4e2dd6a
supriyada <supriyada@gmail.com>
2021-05-03 12:01:04 -0400

    Bonus update

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -3,14 +3,69 @@ var tableData = data;
 
 //console.log(tableData);
 
-var ufoTableBody = d3.select("tbody");
+//adds the list of countries to the select option
+var countrySelect = document.getElementById('country');
+var countryList = [];
+var countryData = tableData.map(countryListFn => countryListFn.country);
+countryData.forEach((country) => {
+    if (!(countryList.includes(country))){
+        
+        countryList.push(country);
+        var opt = document.createElement("option");
+        opt.value = country;
+        opt.text = country;
+        countrySelect.appendChild(opt);
+    }
+});
+console.log(countryList);
+
+
+// To empty the select option
+function removeAll(selectBox,selList) {
+    while (selectBox.options.length > 0) {
+        selectBox.remove(0);
+    }
+}
+
+//generates list of states based on country selected
+var stateList = [];
+var stateSelect = document.getElementById("state");
+function myState(){
+    var countrySel = countrySelect.value;
+
+    removeAll(stateSelect);
+
+    stateList = [];
+
+    console.log("First selection");
+    var ss = tableData.map(function (stateListFn) {
+        if (stateListFn.country == countrySel){
+            console.log("First list append");
+            var state = stateListFn.state;
+            if (!(stateList.includes(state))){
+        
+                stateList.push(state);
+                var opt = document.createElement("option");
+                opt.value = state;
+                opt.text = state;
+                stateSelect.appendChild(opt);
+            }
+        }
+    })
+}
+
+//generates the list of cities based on state selected
+var cityList = [];
+var citySelect = document.getElementById("city");
+function myCity(){}
 
 //appends a table to the web page and then adds new rows of data for each UFO sighting
+var ufoTableBody = d3.select("tbody");
 tableData.forEach( (ufoReport) =>{
     var ufoRow = ufoTableBody.append("tr");
-
+    
     Object.entries(ufoReport).forEach(([key,value]) => {
-        //console.log(key,value);
+        
         var ufoCell = ufoRow.append("td");
         ufoCell.text(value);
     });
@@ -33,6 +88,7 @@ function runEnter(){
     //console.log(inputValue);
     
     var filteredTable = tableData.filter(ufoSightings => ufoSightings.datetime === filterDate);
+
     //console.log(filteredTable);
     ufoTableBody.html("");
 
